9bd4783756e7f050926b64fdb2a71882fec0dc30
Yinnii <y.yin94@outlook.de>
2025-09-12 22:28:06 +0200

    Update to litellm

--- src/similar_data.py
+++ src/similar_data.py
@@ -1,6 +1,6 @@
+import litellm
 import neo4j, os, sys
 import numpy as np
-import requests
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import ontorag_logger as logger
 
@@ -10,18 +10,24 @@ DB_NAME = 'neo4j'
 driver = neo4j.GraphDatabase.driver(URI, auth=AUTH)
 driver.verify_connectivity()
 
-openAI_token = os.getenv("AZURE_API_KEY", None)
-
-search_query = '''
-WITH genai.vector.encode($searchPrompt, 'AzureOpenAI', { token: $token, resource: $resource, deployment: 'text-embedding-3-small'}) AS queryVector
-CALL db.index.vector.queryNodes('embedding', 5, queryVector)
+search_query = f'''
+CALL db.index.vector.queryNodes('embedding', 5, $queryVector)
 YIELD node, score
 RETURN node.name as name, node.description, score
 '''
 
 def find_similar_dataset(search_prompt: str):
+
+    response = litellm.embedding(
+        api_base=os.getenv("OPENAI_ENDPOINT"),
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model=os.getenv("EMBEDDING_MODEL", "text-embedding-3-large"),
+        dimensions=1536,
+        input=search_prompt,
+    )
+
     records, _, _ = driver.execute_query(
-        search_query, searchPrompt=search_prompt, token=openAI_token, resource='gpt-dbis',
+        search_query, queryVector=response['data'][0]['embedding'],
         database_=DB_NAME)
 
     if not records:
@@ -30,43 +36,40 @@ def find_similar_dataset(search_prompt: str):
     logger.info(f"Found {len(records)} similar datasets.")
     records.sort(key=lambda x: x['score'], reverse=True)
 
-    return records[0]
+    tokens = response['usage']['total_tokens']
+    return records[0], tokens
 
 def check_parameters_for_equality(run1, run2):
     """
     Check if two runs have similar parameters.
     """
-    AZURE_API_KEY = os.getenv("AZURE_API_KEY")
-    EMBEDDING_ENDPOINT = os.getenv("EMBEDDING_SMALL")  # or EMBEDDING_LARGE
+    total_token = 0
 
-    headers = {
-        "Content-Type": "application/json",
-        "api-key": AZURE_API_KEY,
-    }
+    response1 = litellm.embedding(
+        api_base=os.getenv("OPENAI_ENDPOINT"),
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model=os.getenv("EMBEDDING_MODEL", "text-embedding-3-large"),
+        dimensions=1536,
+        input=str(run1['run']['flow']['hyperparametersettings']),
+    )
+    total_token += response1['usage']['total_tokens']
 
-    data1 = {
-        "input": str(run1['run']['flow']['hyperparametersettings'])
-    }
+    response2 = litellm.embedding(
+        api_base=os.getenv("OPENAI_ENDPOINT"),
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model=os.getenv("EMBEDDING_MODEL", "text-embedding-3-large"),
+        dimensions=1536,
+        input=str(run2['run']['flow']['hyperparametersettings']),
+    )
+    total_token += response2['usage']['total_tokens']
 
-    data2 = {
-        "input": str(run2['run']['flow']['hyperparametersettings'])
-    }
-
-    response1 = requests.post(EMBEDDING_ENDPOINT, headers=headers, json=data1)
-    response1.raise_for_status()
-    embedding1 = response1.json()["data"][0]["embedding"]
-
-    response2 = requests.post(EMBEDDING_ENDPOINT, headers=headers, json=data2)
-    response2.raise_for_status()
-    embedding2 = response2.json()["data"][0]["embedding"]
-
-    dist = euclidean_distance(embedding1, embedding2)
+    dist = euclidean_distance(response1['data'][0]['embedding'], response2['data'][0]['embedding'])
 
     if dist < 0.1:
         logger.info("Runs have similar hyperparameters.")
-        return True
+        return True, total_token
 
-    return False
+    return False, total_token
 
 def euclidean_distance(vec1, vec2):
     vec1 = np.array(vec1)
